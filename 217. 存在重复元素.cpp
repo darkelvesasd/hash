4dd4b6a702d33621b3114e3285b5f6eabb7fdc9e
darkelvesasd <96123329+darkelvesasd@users.noreply.github.com>
2024-08-16 08:55:01 +0800

    Update 217. 存在重复元素.cpp

--- 217. 存在重复元素.cpp
+++ 217. 存在重复元素.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int> us;
+        unordered_set<int> us;
         int n=nums.size();
         for(int i=1;i<n;i++)
         {
-            us[nums[i-1]]++;
+            us.emplace(nums[i-1]);
             if(us.count(nums[i]))
             {
                 return true;
